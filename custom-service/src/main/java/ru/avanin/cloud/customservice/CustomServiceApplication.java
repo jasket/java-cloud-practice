d4fefc974a6f10e9c02469523e2e225bc94cb772
avanin <avanin@ecocompany.ru>
2018-10-30 10:58:02 +0300

    init

---
+++ custom-service/src/main/java/ru/avanin/cloud/customservice/CustomServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.avanin.cloud.customservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomServiceApplication.class, args);
+    }
+}
