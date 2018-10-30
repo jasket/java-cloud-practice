018fe3e3972ffc3074da30bc25707f6dcd001800
avanin <avanin@ecocompany.ru>
2018-10-30 10:48:48 +0300

    init

---
+++ practice-config/src/test/java/ru/avanin/cloud/practiceconfig/PracticeConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.avanin.cloud.practiceconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticeConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
