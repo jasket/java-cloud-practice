0a34270b820ce133ce76a51204c886f648d938e4
avanin <avanin@ecocompany.ru>
2018-11-16 17:47:28 +0300

    registry-service

--- custom-service/src/main/java/ru/avanin/cloud/customservice/CustomServiceApplication.java
+++ custom-service/src/main/java/ru/avanin/cloud/customservice/CustomServiceApplication.java
@@ -2,7 +2,9 @@ package ru.avanin.cloud.customservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CustomServiceApplication {
 
